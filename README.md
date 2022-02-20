4948fadd03133f897fb0a476b231db09f755fe1a
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2022-02-20 17:06:53 +0100

    Improved function count vectorizer

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ New features for *airpg*
 
 A set of new developments for *airpg* (https://doi.org/10.1186/s12859-021-04309-y)
 
-# List of new features:
+### List of new features:
 1. Identification of genus names in abstracts using sklearn
--- identify_genus_names.py
+++ identify_genus_names.py
@@ -9,7 +9,7 @@ counts and then using pairwise distances to find matches with keywords
 """
 __author__ = 'Michael Gruenstaeudl <m.gruenstaeudl@fu-berlin.de>'
 __info__ = 'Find genus names in abstracts using sklearn'
-__version__ = '2022.02.19.2000'
+__version__ = '2022.02.20.1800'
 
 ##################################################
 # IMPORT OPERATIONS #
@@ -18,14 +18,15 @@ __version__ = '2022.02.19.2000'
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from timeit import default_timer as timer
 #from matplotlib import pyplot
 
 ##################################################
 # READING INPUT #
 #################
 # Setting infiles
-taxon_names_InFn = 'data/Asteraecae_genera.txt'
-scientific_abstracts_InFn = 'data/abstracts_full.txt'
+taxon_names_InFn = '_data_/Asteraecae_genera.txt'
+scientific_abstracts_InFn = '_data_/abstracts_full.txt'
 
 with open(taxon_names_InFn) as f:
     taxon_names = f.readlines()
@@ -33,26 +34,37 @@ taxon_names = ' '.join([n.strip('\n') for n in taxon_names])
 
 with open(scientific_abstracts_InFn) as f:
     scientific_abstracts = f.read().split("\n\n\n")
-scientific_abstracts = [re.sub("\d", "", s) for s in scientific_abstracts]  # remove all digits
-scientific_abstracts = [re.sub("[^a-zA-Z ]", "", s) for s in scientific_abstracts]  # remove all special characters
+scientific_abstracts = [re.sub("\d", " ", a) for a in scientific_abstracts]  # remove all digits
+scientific_abstracts = [re.sub("[^a-zA-Z ]", " ", a) for a in scientific_abstracts]  # remove all special characters
 
 ##################################################
 # FIND MATCHES #
 ################
 
-for idx,abstr in enumerate(scientific_abstracts):
-    compare = [abstr, taxon_names]
-    countVect = CountVectorizer()
-    tokenCountMatrix = countVect.fit_transform(compare)
-    cosSimAsPerc = cosine_similarity(tokenCountMatrix)[0][1] * 100
-    if cosSimAsPerc > 1.0:
-        print("Abstract %s contains %s%% of all genus names" % (idx, str(round(cosSimAsPerc, 2))))
-        names_list = countVect.get_feature_names_out()
+def find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False):
+    ''' Function to find matches via sklearn's CountVectorizer '''
+    results = []
+    for idx,abstr in enumerate(scientific_abstracts):
+        compare = [abstr, taxon_names]
+        countVect = CountVectorizer()
+        tokenCountMatrix = countVect.fit_transform(compare)
+        cosSimAsPerc = cosine_similarity(tokenCountMatrix)[0][1] * 100
+        if verbose:
+            print("Abstract %s: %s%% matches with genus name list" % (str(idx), str(round(cosSimAsPerc, 2))))
+        if cosSimAsPerc > 1.0:
+            if verbose:
+                print("    Abstract %s: Similarity greater than 1%%; saving to output ..." % (str(idx)))
+            results.append(abstr)
+            #names_list = countVect.get_feature_names_out()  # It's not printing out the actual matches
+    return results
 
-        # It's not printing out the actual matches
 
-        print(names_list[0:10])
-
+start = timer()
+results_via_count_vectorizer = find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False)
+end = timer()
+print("\nFinding matches via count vectorizer")
+print("    Number of matching entries found:", len(results_via_count_vectorizer))
+print("    Time elapsed:", round(end-start, 2))
 
 ##################################################
 # EOF #
