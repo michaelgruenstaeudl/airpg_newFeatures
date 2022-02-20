87675b36edefddca6db74275ad5222d9834acd45
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2022-02-20 18:00:49 +0100

    Subdivided code into better functions

--- identify_genus_names.py
+++ identify_genus_names.py
@@ -9,7 +9,7 @@ counts and then using pairwise distances to find matches with keywords
 """
 __author__ = 'Michael Gruenstaeudl <m.gruenstaeudl@fu-berlin.de>'
 __info__ = 'Find genus names in abstracts using sklearn'
-__version__ = '2022.02.20.1800'
+__version__ = '2022.02.20.1830'
 
 ##################################################
 # IMPORT OPERATIONS #
@@ -19,27 +19,26 @@ import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from timeit import default_timer as timer
+import tracemalloc
 #from matplotlib import pyplot
 
 ##################################################
-# READING INPUT #
-#################
-# Setting infiles
-taxon_names_InFn = '_data_/Asteraecae_genera.txt'
-scientific_abstracts_InFn = '_data_/abstracts_full.txt'
-
-with open(taxon_names_InFn) as f:
-    taxon_names = f.readlines()
-taxon_names = ' '.join([n.strip('\n') for n in taxon_names])
-
-with open(scientific_abstracts_InFn) as f:
-    scientific_abstracts = f.read().split("\n\n\n")
-scientific_abstracts = [re.sub("\d", " ", a) for a in scientific_abstracts]  # remove all digits
-scientific_abstracts = [re.sub("[^a-zA-Z ]", " ", a) for a in scientific_abstracts]  # remove all special characters
-
-##################################################
-# FIND MATCHES #
-################
+# FUNCTIONS #
+#############
+def load_and_preprocess(family_name):
+    ''' Function to find matches via sklearn's CountVectorizer '''
+    taxon_names_InFn = '_data_/'+family_name+'_genera.txt'
+    scientific_abstracts_InFn = '_data_/'+family_name+'_abstracts_full.txt'
+    # Load taxon names
+    with open(taxon_names_InFn) as f:
+        taxon_names = f.readlines()
+    taxon_names = ' '.join([n.strip('\n') for n in taxon_names])
+    # Load scientific abstracts
+    with open(scientific_abstracts_InFn) as f:
+        scientific_abstracts = f.read().split("\n\n\n")
+    scientific_abstracts = [re.sub("\d", " ", a) for a in scientific_abstracts]  # remove all digits
+    scientific_abstracts = [re.sub("[^a-zA-Z ]", " ", a) for a in scientific_abstracts]  # remove all special characters
+    return [taxon_names, scientific_abstracts]
 
 def find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False):
     ''' Function to find matches via sklearn's CountVectorizer '''
@@ -58,13 +57,29 @@ def find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False):
             #names_list = countVect.get_feature_names_out()  # It's not printing out the actual matches
     return results
 
+def exec_via_count_vectorizer(scientific_abstracts, taxon_names):
+    ''' Wrapper function to find matches via sklearn's CountVectorizer '''
+    print("Finding matches via count vectorizer")
+    time_start = timer()  # Start time monitoring
+    tracemalloc.start()  # Start memory monitoring
+    results = find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False)
+    time_end = timer()  # End time monitoring
+    results_n = len(results)
+    results_mem = tracemalloc.get_traced_memory()[1] // 1048576
+    tracemalloc.stop()  # Stop memory monitoring
+    results_time = round(time_end-time_start, 2)
+    print("    Number of matching entries found: %s" % (results_n))
+    print("    Memory used at peak: %s MiB" % (results_mem))
+    print("    Time elapsed: %s sec." % (results_time))
+    return [results_n, results_mem, results_time]
 
-start = timer()
-results_via_count_vectorizer = find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False)
-end = timer()
-print("\nFinding matches via count vectorizer")
-print("    Number of matching entries found:", len(results_via_count_vectorizer))
-print("    Time elapsed:", round(end-start, 2))
+##################################################
+# MAIN #
+########
+
+taxon_names, scientific_abstracts = load_and_preprocess("Asteraceae")
+results_n, results_mem, results_time = exec_via_count_vectorizer(scientific_abstracts, taxon_names)
+print([len(scientific_abstracts), results_n, results_mem, results_time])
 
 ##################################################
 # EOF #
