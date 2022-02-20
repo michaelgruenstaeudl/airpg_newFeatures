0423e6ff008a7d1df068dd2141cceaa37354093b
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2022-02-20 20:03:47 +0100

    Adding visualizations

--- README.md
+++ README.md
@@ -4,4 +4,21 @@ New features for *airpg*
 A set of new developments for *airpg* (https://doi.org/10.1186/s12859-021-04309-y)
 
 ### List of new features:
-1. Identification of genus names in abstracts using sklearn
+1. Identification of genus names in scientific abstracts using *sklearn*
+
+![](https://github.com/michaelgruenstaeudl/airpg_newFeatures/blob/master/figure_matches.png)
+
+### Generating input for new features
+
+###### Identification of genus names in scientific abstracts using *sklearn*
+```
+while IFS="" read -u10 -r family || [ -n "$family" ]
+do
+  # Get genus names
+  wget "http://www.theplantlist.org/1.1/browse/A/${family}/${family}.csv"
+  awk -F',' '{print $5}' ${family}.csv | sort -u > ${family}_genera.txt
+  # Get scientific abstracts
+  esearch -db pubmed -query "${family} [TITLE]" | \
+    efetch -format abstract 1>${family}_abstracts_full.txt
+done 10< short_list_families.txt  #angiosperm_families.txt
+```
--- identify_genus_names.py
+++ identify_genus_names.py
@@ -15,36 +15,37 @@ __version__ = '2022.02.20.1830'
 # IMPORT OPERATIONS #
 #####################
 
+import matplotlib.pyplot as plt
+import pandas as pd
 import re
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from timeit import default_timer as timer
 import tracemalloc
-#from matplotlib import pyplot
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 ##################################################
 # FUNCTIONS #
 #############
 def load_and_preprocess(family_name):
     ''' Function to find matches via sklearn's CountVectorizer '''
-    taxon_names_InFn = '_data_/'+family_name+'_genera.txt'
+    genus_names_InFn = '_data_/'+family_name+'_genera.txt'
     scientific_abstracts_InFn = '_data_/'+family_name+'_abstracts_full.txt'
     # Load taxon names
-    with open(taxon_names_InFn) as f:
-        taxon_names = f.readlines()
-    taxon_names = ' '.join([n.strip('\n') for n in taxon_names])
+    with open(genus_names_InFn) as f:
+        genus_names = f.readlines()
+    genus_names = ' '.join([n.strip('\n') for n in genus_names])
     # Load scientific abstracts
     with open(scientific_abstracts_InFn) as f:
         scientific_abstracts = f.read().split("\n\n\n")
     scientific_abstracts = [re.sub("\d", " ", a) for a in scientific_abstracts]  # remove all digits
     scientific_abstracts = [re.sub("[^a-zA-Z ]", " ", a) for a in scientific_abstracts]  # remove all special characters
-    return [taxon_names, scientific_abstracts]
+    return [genus_names, scientific_abstracts]
 
-def find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False):
+def find_via_count_vectorizer(scientific_abstracts, genus_names, verbose=False):
     ''' Function to find matches via sklearn's CountVectorizer '''
     results = []
     for idx,abstr in enumerate(scientific_abstracts):
-        compare = [abstr, taxon_names]
+        compare = [abstr, genus_names]
         countVect = CountVectorizer()
         tokenCountMatrix = countVect.fit_transform(compare)
         cosSimAsPerc = cosine_similarity(tokenCountMatrix)[0][1] * 100
@@ -57,12 +58,12 @@ def find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False):
             #names_list = countVect.get_feature_names_out()  # It's not printing out the actual matches
     return results
 
-def exec_via_count_vectorizer(scientific_abstracts, taxon_names):
+def exec_via_count_vectorizer(family_name, genus_names, scientific_abstracts):
     ''' Wrapper function to find matches via sklearn's CountVectorizer '''
-    print("Finding matches via count vectorizer")
+    print("Finding matches via count vectorizer for %s" % (family_name))
     time_start = timer()  # Start time monitoring
     tracemalloc.start()  # Start memory monitoring
-    results = find_via_count_vectorizer(scientific_abstracts, taxon_names, verbose=False)
+    results = find_via_count_vectorizer(scientific_abstracts, genus_names, verbose=False)
     time_end = timer()  # End time monitoring
     results_n = len(results)
     results_mem = tracemalloc.get_traced_memory()[1] // 1048576
@@ -77,9 +78,47 @@ def exec_via_count_vectorizer(scientific_abstracts, taxon_names):
 # MAIN #
 ########
 
-taxon_names, scientific_abstracts = load_and_preprocess("Asteraceae")
-results_n, results_mem, results_time = exec_via_count_vectorizer(scientific_abstracts, taxon_names)
-print([len(scientific_abstracts), results_n, results_mem, results_time])
+family_names_InFn = '_data_/short_list_families.txt'  # '_data_/list_angiosperm_families.txt'
+with open(family_names_InFn) as f:
+    family_names = f.readlines()
+family_names = [n.strip('\n') for n in family_names]
+
+df = pd.DataFrame(columns=['Family', 'N_Abstracts', 'N_Matches', 'Peak_MEM', 'Time'])
+for idx,fam in enumerate(family_names):
+    genus_names, scientific_abstracts = load_and_preprocess(fam)
+    results_n, results_mem, results_time = exec_via_count_vectorizer(fam, genus_names, scientific_abstracts)
+    values_to_add = {
+        'Family': fam,
+        'N_Abstracts': len(scientific_abstracts),
+        'N_Matches': results_n,
+        'Peak_MEM': results_mem,
+        'Time': results_time
+    }
+    row_to_add = pd.Series(values_to_add, name=idx)
+    df = df.append(row_to_add)
+
+
+##################################################
+# PLOTTING #
+############
+
+plt.figure(figsize=(18,4))
+plt.bar(df["Family"], df["N_Matches"])
+plt.ylabel("Number of matches")
+plt.xticks(rotation=90)
+plt.savefig('figure_matches.png', bbox_inches='tight')
+
+plt.figure(figsize=(18,4))
+plt.bar(df["Family"], df["Time"], color = "red")
+plt.ylabel("Calculation time")
+plt.xticks([])
+plt.savefig('figure_time.png', bbox_inches='tight')
+
+plt.figure(figsize=(18,4))
+plt.bar(df["Family"], df["Peak_MEM"], color = "green")
+plt.ylabel("Peak memory")
+plt.xticks([])
+plt.savefig('figure_mem.png', bbox_inches='tight')
 
 ##################################################
 # EOF #
